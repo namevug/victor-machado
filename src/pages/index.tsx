6a902f37972e100c64dfea253764bfac7095fa2a
namevug <ciavug@gmail.com>
2023-05-10 20:51:59 -0300

    adjusts: Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Header from '@/components/header'
-import Projects from '@/components/projects'
-import { Text, Flex, Box, Image, HStack } from '@chakra-ui/react'
+import Header from '@/components/Header'
+import Projects from '@/components/Projects'
+import { Flex, VStack } from '@chakra-ui/react'
 
 export default function Home() {
   return (
@@ -9,13 +9,57 @@ export default function Home() {
         maxWidth='80rem'
         justifyContent='space-between'
         flexDirection={['column', 'column', 'column', 'row']}
-        mt='5rem'
-        ml='5rem'
-        mr='5rem'
+        m={['2rem', '2rem', '3rem', '3rem', '6rem']}
       >
         <Header />
 
-        <Projects />
+        <Projects
+          date='2023 - Now'
+          title='MentorGo.'
+          description='Com o MentorGo, mentores podem gerenciar de forma eficiente o
+          desenvolvimento individual de cada aluno. A plataforma oferece recursos
+          personalizados, permitindo que os mentores criem desafios, escrevam
+          artigos, cadastrem cursos e criem salas. Se você já presta algum tipo de
+          mentoria, aumente a eficiência de seus ensinamentos e leve seus alunos
+          ao próximo nível com o MentorGo.'
+          href='View in Web'
+          image='/projects/mentorgo.png'
+        />
+
+        {/* Exemplo de como deverá ficar com mais cards */}
+        {/* <VStack
+          h='75vh'
+          ml='2rem'
+          gap='3.875rem'
+          overflowY='scroll'
+          sx={{
+            '&::-webkit-scrollbar': {
+              width: '0.5rem',
+              height: '0.5rem',
+            },
+          }}
+        >
+          <Projects
+            date='2023 - Now'
+            title='MentorGo.'
+            description='Com o MentorGo, mentores podem gerenciar de forma eficiente o
+          desenvolvimento individual de cada aluno. A plataforma oferece recursos
+          personalizados, permitindo que os mentores criem desafios, escrevam
+          artigos, cadastrem cursos e criem salas. Se você já presta algum tipo de
+          mentoria, aumente a eficiência de seus ensinamentos e leve seus alunos
+          ao próximo nível com o MentorGo.'
+          />
+          <Projects
+            date='2023 - Now'
+            title='MentorGo.'
+            description='Com o MentorGo, mentores podem gerenciar de forma eficiente o
+          desenvolvimento individual de cada aluno. A plataforma oferece recursos
+          personalizados, permitindo que os mentores criem desafios, escrevam
+          artigos, cadastrem cursos e criem salas. Se você já presta algum tipo de
+          mentoria, aumente a eficiência de seus ensinamentos e leve seus alunos
+          ao próximo nível com o MentorGo.'
+          />
+        </VStack> */}
       </Flex>
     </>
   )
