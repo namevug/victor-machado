511b9d2b0abf6cf571a0828292dbf134027d75fa
namevug <ciavug@gmail.com>
2023-05-10 12:00:51 -0300

    adjust: Description, Hover the Img

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -19,9 +19,12 @@ export default function Projects() {
         MentorGo.
       </Text>
       <Text mt='0.313rem' fontSize='0.875rem'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-        reiciendis necessitatibus eos voluptatum debitis repellat excepturi enim
-        quam, ea deleniti facilis, quasi nesciunt.
+        Com o MentorGo, mentores podem gerenciar de forma eficiente o
+        desenvolvimento individual de cada aluno. A plataforma oferece recursos
+        personalizados, permitindo que os mentores criem desafios, escrevam
+        artigos, cadastrem cursos e criem salas. Se você já presta algum tipo de
+        mentoria, aumente a eficiência de seus ensinamentos e leve seus alunos
+        ao próximo nível com o MentorGo.
       </Text>
       <Text mt='0.563rem' mb='0.563rem' fontSize='0.875rem' fontWeight='600'>
         NextJs ChakraUi MongoDb
@@ -30,7 +33,13 @@ export default function Projects() {
         <a href='#'>View in Github</a>
       </Text>
 
-      <Box maxWidth='28.75rem' mt='0.75rem'>
+      <Box
+        maxWidth='28.75rem'
+        mt='0.75rem'
+        cursor='pointer'
+        _hover={{ transform: 'scale(1.06)' }}
+        transition='transform 0.2s ease-in-out'
+      >
         <Image src='/projects/mentorgo.png' />
       </Box>
     </Box>
