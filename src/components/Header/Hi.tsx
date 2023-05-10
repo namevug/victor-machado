4acea8e1e9c508c81bb0eaf8f242a7bd64dfba03
namevug <ciavug@gmail.com>
2023-05-10 20:45:28 -0300

    feat: Created Hi-Component

---
+++ src/components/Header/Hi.tsx
@@ -0,0 +1,16 @@
+import { Box, Text } from '@chakra-ui/react'
+
+export default function Hi() {
+  return (
+    <Box
+      w='100%'
+      mt='2.5rem'
+      fontSize={['38px', '38px', '48px']}
+      fontWeight={'bold'}
+      className='gradient-text'
+    >
+      <Text title='Hi, I’m'>Hi, I’m</Text>
+      <Text title='Victor Machado.'>Victor Machado.</Text>
+    </Box>
+  )
+}
