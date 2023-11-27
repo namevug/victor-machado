dc8659267668a52dc8c492109eab316a213333cc
namevug <ciavug@gmail.com>
2023-11-27 20:44:52 -0300

    feat: confidentialProject

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,14 +21,14 @@ export default function Home() {
   }, []);
 
   const project = [
-    {
-      date: "2023 - Now",
-      title: "Verbaliza Cx.",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-      stacks: ["Next.js", "Tailwind", "PostgreSQL"],
-      href: "https://verbalizacx.com/",
-      image: "/projects/verbalizapp.png",
-    },
+    // {
+    //   date: "2023 - Now",
+    //   title: "Verbaliza Cx.",
+    //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    //   stacks: ["Next.js", "Tailwind", "PostgreSQL"],
+    //   href: "https://verbalizacx.com/",
+    //   image: "/projects/verbalizapp.png",
+    // },
     {
       date: "2023 Jan - Apr",
       title: "MentorGo.",
